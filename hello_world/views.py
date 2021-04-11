e2f0772f815aff2d106457d09b157cb72c45a829
amisha <kothawadeamisha20@gmail.com>
2021-04-11 16:55:28 +0530

    my first commit

---
+++ hello_world/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render,Httpresponse
+
+# Create your views here.
+def show(request):
+    return Httpresponse('Hello,This is Amisha kothawade from T2 batch my PRN is 1841025')
